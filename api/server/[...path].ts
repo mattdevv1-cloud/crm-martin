e287c87119459409f14a925c63fc2ad6f5a5d1e9
yakulevichvlad11 <yakulevich.work@gmail.com>
2025-10-29 17:37:21 +0300

    Fix Edge Function import path for Vercel

--- api/server/[...path].ts
+++ api/server/[...path].ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
-import * as kv from '@/server/kv_store';
+import * as kv from '../../src/server/kv_store';
 
 const app = new Hono();
 
